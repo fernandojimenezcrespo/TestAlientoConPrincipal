047316cf8b35c43c1681a21390d7a82f52a08547
fernandojimenezcrespo <73723133+fernandojimenezcrespo@users.noreply.github.com>
2021-11-09 11:48:25 +0100

    SEGUNDO INTENTO

--- src/sftp/SftpCanal.java
+++ src/sftp/SftpCanal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sftp;
+package sftp;  
   
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
